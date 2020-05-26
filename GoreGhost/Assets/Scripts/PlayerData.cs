a38f358c4ce98a6492330bcad659cf4611b04649
Falko <langmackf@gmail.com>
2020-05-26 12:44:38 +0200

    * PlayerData sript und object fuers tracken des spiel fortschritts; * build settings updated

---
+++ GoreGhost/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.UCI307.GOREGHOST3
+{
+    [CreateAssetMenu(fileName = "New PlayerData", menuName = "GoreGhost3/PlayerData")]
+    public class PlayerData : ScriptableObject
+    {
+        #region Public Fields
+        public string playerName;
+        #endregion
+    }
+}
