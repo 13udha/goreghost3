08f4e426084e60ac9036a48006b46ad1b0f89e58
Falko <langmackf@gmail.com>
2020-05-30 19:55:58 +0200

    * added dataObjects for 4 players in multiplayer * added dataObject for playable characters * added art for two more characters * startet work on multiplayer ui display

--- GoreGhost/Assets/Scripts/Characters/CharacterObject.cs
+++ GoreGhost/Assets/Scripts/Characters/CharacterObject.cs
@@ -7,6 +7,19 @@ namespace Com.UCI307.GOREGHOST3
     [CreateAssetMenu(fileName = "New CharacterData", menuName = "GoreGhost3/GameContent/CharacterData")]
     public class CharacterObject : ScriptableObject
     {
-        
+        #region PublicFields
+        [Header("Attributes")]
+        public string characterName;
+        public float health;
+        public float energy;
+
+        [Header("Additional Attributes")]
+        [TextArea]
+        public string description;
+
+        [Header("Configuratio")]
+        public bool isUnlocked;
+        public GameObject prefab;
+        #endregion
     }
 }
--- GoreGhost/Assets/Scripts/Player/MutiplayerData.cs
+++ GoreGhost/Assets/Scripts/Player/MutiplayerData.cs
@@ -7,6 +7,13 @@ namespace Com.UCI307.GOREGHOST3
     [CreateAssetMenu(fileName = "New MultiplayerData", menuName = "GoreGhost3/Player/MultiPlayerData")]
     public class MutiplayerData : ScriptableObject
     {
-        
+        #region PublicFields
+        [Header("Configuration")]
+        public bool isPlaying;
+
+        [Tooltip("Differentiates between the four players")]
+        public int playerID;
+        public Color playerColo;
+        #endregion
     }
 }
