be69d4e4cf26b4a5cf45d4a7af7f9f6601f86559
Falko <langmackf@gmail.com>
2020-05-30 17:29:16 +0200

    * GameLevelData Object for Information to levels * GameLeveButton Script for integragtion of game level data ** Both integrated in MainMenu

---
+++ GoreGhost/Assets/Scripts/UI/MainMenu/LevelButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Com.UCI307.GOREGHOST3 {
+    public class LevelButton : MonoBehaviour
+    {
+        #region Public Fields
+        [Header("Dependencys")]
+        public GameLevelData data;
+        [Header("UI Components")]
+        public Text levelNameDisplay;
+        #endregion
+
+        #region MonoBehaviour Callbacks
+        // Start is called before the first frame update
+        void Start()
+        {
+            if(data == null)
+            {
+                Debug.LogError(this.ToString() + " Level Load Button has not been given a Level Data Object!");
+            }
+            else
+            {
+                SetUpButton();
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        #endregion
+
+        #region Public Methods
+
+        public void StartLevel()
+        {
+            SceneManager.LoadScene(data.sceneName);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void SetUpButton()
+        {
+            levelNameDisplay.text = data.levelName;
+        }
+
+        #endregion
+    }
+}
