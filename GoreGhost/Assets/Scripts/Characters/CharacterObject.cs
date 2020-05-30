b29dff8db427b005e896931c0e4e3d70c83d3ab9
Falko <langmackf@gmail.com>
2020-05-30 19:18:06 +0200

    * added scriptable object for multiplayer data * added scriptable object for character data * improved create asset emnu integration

---
+++ GoreGhost/Assets/Scripts/Characters/CharacterObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.UCI307.GOREGHOST3
+{
+    [CreateAssetMenu(fileName = "New CharacterData", menuName = "GoreGhost3/GameContent/CharacterData")]
+    public class CharacterObject : ScriptableObject
+    {
+        
+    }
+}
---
+++ GoreGhost/Assets/Scripts/Player/MutiplayerData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.UCI307.GOREGHOST3
+{
+    [CreateAssetMenu(fileName = "New MultiplayerData", menuName = "GoreGhost3/Player/MultiPlayerData")]
+    public class MutiplayerData : ScriptableObject
+    {
+        
+    }
+}
